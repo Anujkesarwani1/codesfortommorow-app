fc9c3a8ff8b343d94cbaa94096e9f819484e2f52
Anuj Kesarwani <anuj.kesarwani@zemosolabs.com>
2023-10-21 13:30:45 +0530

    React app setup

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import MuiTypography from 'components/atoms/Typography'
+import './style.css'
+
+export const App = () => {
+  return (
+    <MuiTypography variant='body3' children='Hello World'/>
+  )
+}
